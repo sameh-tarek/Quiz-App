84055670e872ee9a6f34bc62e8974deeb3f852d8
Sameh1Tarek <Sameh1Tarek>
2023-09-05 06:18:04 +0300

    add logs

--- src/main/java/com/sameh/quizapp/controller/QuestionController.java
+++ src/main/java/com/sameh/quizapp/controller/QuestionController.java
@@ -2,6 +2,8 @@ package com.sameh.quizapp.controller;
 
 import com.sameh.quizapp.model.Question;
 import com.sameh.quizapp.service.QuestionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,31 +14,38 @@ import java.util.List;
 @RequestMapping("question")
 public class QuestionController {
 
+    private static final Logger logger = LoggerFactory.getLogger(QuestionController.class);
+
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("allQuestions")
     public ResponseEntity<List<Question>> getAllQuestion(){
+        logger.info("========== inside getAllQuestion method ===========");
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
     public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+        logger.info("============ inside getQuestionsByCategory method =============");
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
     public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        logger.info("========== inside addQuestion method ===========");
         return questionService.addQuestion(question);
     }
 
     @PutMapping("update/{id}")
     public String updateQuestion(@PathVariable Integer id, @RequestBody Question question){
+        logger.info("=========== inside updateQuestion method ============");
         return questionService.updateQuestion(id,question);
     }
 
     @DeleteMapping("delete/{id}")
     public String deleteQuestion(@PathVariable Integer id ){
+        logger.info("============= inside deleteQuestion method ==============");
         return questionService.deleteQuestion(id);
     }
 
--- src/main/java/com/sameh/quizapp/service/impl/QuizServiceImpl.java
+++ src/main/java/com/sameh/quizapp/service/impl/QuizServiceImpl.java
@@ -8,6 +8,8 @@ import com.sameh.quizapp.model.QuestionWrapper;
 import com.sameh.quizapp.model.Quiz;
 import com.sameh.quizapp.model.Response;
 import com.sameh.quizapp.service.QuizService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,8 @@ import java.util.Optional;
 @Service
 public class QuizServiceImpl implements QuizService {
 
+    private static final Logger logger = LoggerFactory.getLogger(QuizServiceImpl.class);
+
     @Autowired
     QuizDao quizDao;
     @Autowired
@@ -27,15 +31,21 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
+        logger.info("=========== inside createQuiz method ==========");
         if(numQ<=0){
+            logger.warn("Cannot create quiz, number of questions must be greater than 0");
             throw new ApiNotFoundException("A quiz must contain one or more questions");
         }
 
         List<Question> questions = questionDao.findRandomQuestionsByCategory(category, numQ);
-        if(questions.isEmpty())
+        if(questions.isEmpty()){
+            logger.error("Cannot create quiz, no questions found in category: {}", category);
             throw new ApiNotFoundException("Not Found Questions in this category: "+category);
+        }
+
 
         if(questions.size()<numQ){
+            logger.error("Cannot create quiz, there are only {} questions in {} category and you choose {}", questions.size(), category, numQ);
             throw new ApiNotFoundException("There are only "+ questions.size() +" questions in "+ category +" category and you choose "+numQ);
         }
 
@@ -44,14 +54,18 @@ public class QuizServiceImpl implements QuizService {
         quiz.setQuestions(questions);
         quizDao.save(quiz);
 
+        logger.info("Quiz created successfully");
         return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        logger.info("========== inside getQuizQuestions method =========");
         Optional<Quiz> quiz = quizDao.findById(id);
-        if(quiz.isEmpty())
+        if(quiz.isEmpty()){
+            logger.warn("Cannot get quiz, quiz with id: {} not found", id);
             throw new ApiNotFoundException("not found quiz with this id: "+id);
+        }
 
         List<Question> questionsFromDb = quiz.get().getQuestions();
         List<QuestionWrapper> questionsForUsers = new ArrayList<>();
@@ -64,9 +78,13 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        logger.info("========== inside calculateResult method =========");
+
         Optional<Quiz> quiz = quizDao.findById(id);
-        if(quiz.isEmpty())
+        if(quiz.isEmpty()){
+            logger.warn("Cannot calculate results, quiz with id: {} not found", id);
             throw new ApiNotFoundException("not found quiz with this id: "+id);
+        }
 
         List<Question> questions = quiz.get().getQuestions();
         int right = 0;
@@ -77,6 +95,8 @@ public class QuizServiceImpl implements QuizService {
             }
             i++;
         }
+
+        logger.info("Quiz submitted successfully and the result is {}",right);
         return new ResponseEntity<>(right, HttpStatus.OK);
     }
 }
